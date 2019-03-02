9bc8348265239582ee412280103a9cc67cfdd184
douglaslbittencourt <douglaslbittencourt@gmail.com>
2019-03-02 11:19:33 -0300

    Concertado endpoins para a classe produto

--- src/main/java/com/douglaslbittencourt/cursomc/services/CategoriaService.java
+++ src/main/java/com/douglaslbittencourt/cursomc/services/CategoriaService.java
@@ -8,33 +8,35 @@ import org.springframework.stereotype.Service;
 
 import com.douglaslbittencourt.cursomc.domain.Categoria;
 import com.douglaslbittencourt.cursomc.repositories.CategoriaRepository;
+import com.douglaslbittencourt.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		repo.deleteById(id);
 	}
-	
+
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
