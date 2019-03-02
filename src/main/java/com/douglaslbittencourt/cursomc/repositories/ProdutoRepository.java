fab20a68ee2a26ea918fec3b8a49fba4f8afea49
douglaslbittencourt <douglaslbittencourt@gmail.com>
2019-03-02 10:54:33 -0300

    Adicionado produtos

---
+++ src/main/java/com/douglaslbittencourt/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.douglaslbittencourt.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.douglaslbittencourt.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
